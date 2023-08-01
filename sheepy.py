1cd9d10b014122d047a0f1813aeac61647c75f36
perinze <perinzefaper@foxmail.com>
2023-08-01 19:45:04 +0800

    init parser

--- sheepy.py
+++ sheepy.py
@@ -15,29 +15,46 @@ t_EMPTY = r'\s+'
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-class Comment:
-    def __init__(self, content: str):
-        self.content = content
-
-class SQuote:
-    def __init__(self, content: str):
-        self.content = content
-
-class Newline:
-    pass
-
-class Assign:
-    def __init__(self, name: str, value: str):
-        self.name = name
-        self.value = value
-
-class Var:
-    def __init__(self, name: str):
-        self.name = name
-
 class Word:
     def __init__(self, str: str):
         self.str = str
+    
+    def is_word(obj: object) -> bool:
+        return isinstance(obj, Word)
+    
+    def is_word_with(obj: object, str: str) -> bool:
+        if not Word.is_word(obj):
+            return False
+        elif obj.str == str:
+            return True
+        return False
+
+class Comment:
+    def __init__(self, content: str):
+        self.content = content
+    
+    def is_comment(obj: object) -> bool:
+        return isinstance(obj, Comment)
+
+class SQuote(Word):
+    def __init__(self, str: str, content: str):
+        super().__init__(str)
+        self.content = content
+
+class Newline:
+    def is_newline(obj: object) -> bool:
+        return isinstance(obj, Newline)
+
+class Assign(Word):
+    def __init__(self, str: str, name: str, value: str):
+        super().__init__(str)
+        self.name = name
+        self.value = value
+
+class Var(Word):
+    def __init__(self, str: str, name: str):
+        super().__init__(str)
+        self.name = name
 
 class Lexer:
     def __init__(self, input: str):
@@ -91,7 +108,7 @@ class Lexer:
         if m == None:
             return False
         elif m.span()[0] == 0:
-            self.token.append(SQuote(m.group(1)))
+            self.token.append(SQuote(m.group(0), m.group(1)))
             self.cut(m.span())
             return True
         return False
@@ -111,7 +128,7 @@ class Lexer:
         if m == None:
             return False
         elif m.span()[0] == 0:
-            self.token.append(Assign(m.group(1), m.group(2)))
+            self.token.append(Assign(m.group(0), m.group(1), m.group(2)))
             self.cut(m.span())
             return True
         return False
@@ -121,7 +138,7 @@ class Lexer:
         if m == None:
             return False
         elif m.span()[0] == 0:
-            self.token.append(Var(m.group(1)))
+            self.token.append(Var(m.group(0), m.group(1)))
             self.cut(m.span())
             return True
         return False
@@ -131,7 +148,7 @@ class Lexer:
         if m == None:
             return False
         elif m.span()[0] == 0:
-            self.token.append(Var(m.group(1)))
+            self.token.append(Var(m.group(0), m.group(1)))
             self.cut(m.span())
             return True
         return False
@@ -161,17 +178,61 @@ class Lexer:
     def cut(self, span: tuple[int, int]):
         self.input = self.input[span[1]:]
 
+class Stmt:
+    def __init__(self, comment: Comment):
+        self.comment = comment
+
+class Echo(Stmt):
+    def __init__(self, comment: Comment, args: list[Word]):
+        super().__init__(comment)
+        self.args = args
+
+class Parser:
+    def __init__(self, token: list[object]):
+        self.token = token
+        self.stmt = []
+    
+    def parse(self):
+        while True:
+            if self.token == []:
+                eprint("parse done")
+                return self.stmt
+            elif self.parse_echo():
+                continue
+            else:
+                eprint("parse failed")
+                return None
+    
+    def parse_echo(self):
+        i = 0
+        comment = None
+        if Word.is_word_with(self.token[i], "echo"):
+            args = []
+            i += 1
+            while Word.is_word(self.token[i]):
+                args.append(self.token[i])
+                i += 1
+            if Comment.is_comment(self.token[i]):
+                comment = self.token[i]
+                i += 1
+            if Newline.is_newline(self.token[i]):
+                i += 1
+            self.stmt.append(Echo(comment, args))
+            self.cut(i)
+            return True
+        else:
+            return False
+    
+    def cut(self, i: int):
+        self.token = self.token[i:]
+
 class Transpiler:
-    def __init__(self, input: str):
-        self.input = input
+    def __init__(self):
         self.header = "#!/usr/bin/python3 -u\n"
         self.body = ""
         self.token = []
 
     def transpile(self) -> str:
-        lexer = Lexer(self.input)
-        self.token = lexer.tokenize()
-        eprint(self.token)
         return self.header + self.body
 
 if __name__ == '__main__':
@@ -180,5 +241,9 @@ if __name__ == '__main__':
         exit(0)
     filename = sys.argv[1]
     with open(filename) as f:
-        transpiler = Transpiler(f.read())
-        print(transpiler.transpile())
+        lexer = Lexer(f.read())
+        token = lexer.tokenize()
+        eprint(token)
+        parser = Parser(token)
+        stmt = parser.parse()
+        eprint(stmt)
